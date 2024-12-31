74ae4685db1634bd8a721a171039239a5fdd18a7
Fahad Abdullah <134834352+FAbdullah17@users.noreply.github.com>
2024-12-31 19:45:30 +0500

    Final Commit

--- setup.py
+++ setup.py
@@ -6,10 +6,10 @@ with open("README.md", "r", encoding="utf-8") as f:
 
 __version__ = "0.0.0"
 
-REPO_NAME = "Text-Summarizer-Project"
-AUTHOR_USER_NAME = "entbappy"
+REPO_NAME = "End-to-End-Text-Summarizer"
+AUTHOR_USER_NAME = "FAbdullah17"
 SRC_REPO = "textSummarizer"
-AUTHOR_EMAIL = "entbappy73@gmail.com"
+AUTHOR_EMAIL = "fahadai.co@gmail.com"
 
 
 
